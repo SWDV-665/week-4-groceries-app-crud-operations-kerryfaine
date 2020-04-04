f4b7814a338d475d88760ec65557eae03312d4c0
kerryfaine <46699746+kerryfaine@users.noreply.github.com>
2020-04-04 17:57:50 -0500

    Week 4 grocery app edits
    centralized the CRUD operations, applied some styles, and centralized the pop data entry screens.

---
+++ groceries/src/pages/home/home.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+    constructor(public navCtrl: NavController, public toastCtrl: ToastController,  public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider ) {
+
+  }
+
+  loadItems()
+  {
+    return this.dataService.getItems(); 
+  }
+
+  removeItem(item, index) {
+  
+    const toast = this.toastCtrl.create({
+      message: 'Removing ' + item.name + " ...",
+      duration: 3000
+    });
+
+    toast.present(); 
+    this.dataService.removeItem(index);
+  }
+
+  editItem(item, index) {
+
+    const toast = this.toastCtrl.create({
+      message: 'Editing ' + item.name + " ...",
+      duration: 3000
+    });
+
+    toast.present(); 
+    this.inputDialogService.showPrompt(item,index);
+  }
+
+  addItem() {
+    this.inputDialogService.showPrompt(); 
+  }
+
+ 
+}
